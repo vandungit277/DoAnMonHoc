06b657d129ab0221836fafb84703627fe4809f3b
vandungit277 <vandungit277@gmail.com>
2016-12-23 20:08:35 +0700

    toi vua moi tao moi file project

---
+++ DoAnMonHoc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAnMonHoc.Startup))]
+namespace DoAnMonHoc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
