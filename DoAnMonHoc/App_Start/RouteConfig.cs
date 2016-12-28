99474c4f5ae05bf19701b806832c1f98e9c70256
Quoc Toan <Mail.Toan95@gmail.com>
2016-12-28 23:35:44 +0700

    Hoàn tất các trang quản lý nhà cung cấp

--- DoAnMonHoc/App_Start/RouteConfig.cs
+++ DoAnMonHoc/App_Start/RouteConfig.cs
@@ -14,6 +14,36 @@ namespace DoAnMonHoc
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "Danh sách nhà cung cấp",
+               url: "admin/danh-sach-nha-cung-cap",
+               defaults: new { controller = "Admin", action = "DS_NhaCungCap", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "Thêm mới nhà cung cấp",
+               url: "admin/them-moi-nha-cung-cap",
+               defaults: new { controller = "Admin", action = "Themmoincc", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+              name: "Chi tiết nhà cung cấp",
+              url: "admin/chi-tiet-nha-cung-cap-id-{id}",
+              defaults: new { controller = "Admin", action = "Chitietncc", id = UrlParameter.Optional }
+              );
+
+            routes.MapRoute(
+              name: "Xóa nhà cung cấp",
+              url: "admin/xoa-nha-cung-cap-id-{id}",
+              defaults: new { controller = "Admin", action = "Xoancc", id = UrlParameter.Optional }
+              );
+
+            routes.MapRoute(
+              name: "Sửa nhà cung cấp",
+              url: "admin/sua-nha-cung-cap-id-{id}",
+              defaults: new { controller = "Admin", action = "Suancc", id = UrlParameter.Optional }
+              );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional }
